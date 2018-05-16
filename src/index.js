d867795825cbfaebc089430dce9fe1cd166d95fd
Sergio Garcia Santos <sergio@sergiogarcias.com>
2018-05-16 11:06:39 +0200

    style: use single cuotes instead double in module imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Switch, Route, browserHistory } from "react-router-dom";
-import promises from "redux-promise";
-import reducers from "./reducers";
-import Home from "./components/home.jsx";
-import UserProfile from "./components/userProfile.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Switch, Route, browserHistory } from 'react-router-dom';
+import promises from 'redux-promise';
+import reducers from './reducers';
+import Home from './components/home.jsx';
+import UserProfile from './components/userProfile.jsx';
 
 const createStoreWithMiddleware = applyMiddleware(promises)(createStore);
 
@@ -19,5 +19,5 @@ ReactDOM.render(
       </Switch>
     </BrowserRouter>
   </Provider>,
-  document.querySelector(".container")
+  document.querySelector('.container'),
 );
